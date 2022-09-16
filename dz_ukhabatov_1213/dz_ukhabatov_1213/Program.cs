72aa332ede56603af89d59a158a47bf8393c475c
wolf <ukhabatov@mail.ru>
2022-09-16 21:30:11 +0300

    dz 16.09.2022

--- dz_ukhabatov_1213/dz_ukhabatov_1213/Program.cs
+++ dz_ukhabatov_1213/dz_ukhabatov_1213/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using dz_ukhabatov_1213;
+
+class Program
+{
+    public static int Main()
+    {
+        var c = new dz_16_09();
+
+        Console.WriteLine("Составить таблицу значений функции y = 5 - x2/2 на отрезке [-5; 5] с шагом 0.5.\n");
+        c.table_func();
+
+        Console.WriteLine("Ряд Фибоначи.\n");
+        c.fibonachi();
+
+        Console.WriteLine("Поиск числа.\n");
+        c.findNumber();
+        return 0;
+    }
+}
---
+++ dz_ukhabatov_1213/dz_ukhabatov_1213/dz_16_09.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz_ukhabatov_1213
+{
+    public class dz_16_09
+    {
+        double counter(double x)
+        {
+            if (x <= 5)
+            {
+                var y = (5 - x * 2 / 2);
+
+                Console.WriteLine($"y = {y}, x = {x}");
+
+                return this.counter(x + 0.5);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        public void table_func()
+        {
+            this.counter(-5);
+        }
+
+        public void fibonachi()
+        {
+            var x = 0;
+            var y = 1;
+            var z = 0;
+
+            while (y <= 100)
+            {
+                z = y;
+                y = x + y;
+                x = z;
+                Console.WriteLine(x);
+            }
+        }
+
+        public int findNumber()
+        {
+            Console.Write("Найти числа от: ");
+            var a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("До: ");
+            var b = Convert.ToInt32(Console.ReadLine());
+
+            if (a > b)
+            {
+                Console.WriteLine("Ошибка. Первое чило не может быть больше второго!");
+                return 0;
+            }
+
+            Random r = new Random();
+            int c = r.Next(a, b);
+
+            int v;
+
+            do
+            {
+                v = Convert.ToInt32(Console.ReadLine());
+                if (v > c)
+                {
+                    Console.WriteLine("Заданное число меньше");
+                }
+                else if (v != c)
+                {
+                    Console.WriteLine("Заданное число больше");
+                }
+
+            } while (v != c);
+            Console.WriteLine("Вы угадали число!");
+            return 1;
+        }
+    }
+}
